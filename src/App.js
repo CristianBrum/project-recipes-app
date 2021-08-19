0ae46296f9bdd7d673aa5f6830c984c0091d79ff
Cristian Brum Oliveira <cristian.brum2@yahoo.com.br>
2021-08-19 03:51:37 -0300

    testa gh page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Routes from './helpers/Routes';
 import RecipesProvider from './context/RecipesProvider';
+import { HashRouter } from 'react-router-dom';
 
 function App() {
   return (
+    <HashRouter basename={process.env.PUBLIC_URL}>
     <RecipesProvider>
       <Routes />
     </RecipesProvider>
+    </HashRouter>
   );
 }
 
