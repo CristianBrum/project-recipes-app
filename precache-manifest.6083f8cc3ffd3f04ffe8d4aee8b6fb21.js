adb84429db667c817b5802721063641017035d07
Cristian Brum Oliveira <cristian.brum2@yahoo.com.br>
2021-08-19 01:33:29 -0300

    Updates

---
+++ precache-manifest.6083f8cc3ffd3f04ffe8d4aee8b6fb21.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45b321e48a024e3ebf902fe1b4532762",
+    "url": "/cristianbrum.io/projects/index.html"
+  },
+  {
+    "revision": "4d451629cdc4cdef5a04",
+    "url": "/cristianbrum.io/projects/static/css/2.95258f03.chunk.css"
+  },
+  {
+    "revision": "6ea40e29a15f1f391dcc",
+    "url": "/cristianbrum.io/projects/static/css/main.8a6ba23a.chunk.css"
+  },
+  {
+    "revision": "4d451629cdc4cdef5a04",
+    "url": "/cristianbrum.io/projects/static/js/2.d14d7e42.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/cristianbrum.io/projects/static/js/2.d14d7e42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ea40e29a15f1f391dcc",
+    "url": "/cristianbrum.io/projects/static/js/main.cec58785.chunk.js"
+  },
+  {
+    "revision": "9d69a96226c8ad717dbe",
+    "url": "/cristianbrum.io/projects/static/js/runtime-main.77b5a5ba.js"
+  },
+  {
+    "revision": "713fe29d10415a51910d79f66c21d161",
+    "url": "/cristianbrum.io/projects/static/media/arrowLeft.713fe29d.svg"
+  },
+  {
+    "revision": "e815ef6d37c3b8e5045a919688e9db2a",
+    "url": "/cristianbrum.io/projects/static/media/backLogin.e815ef6d.jpg"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/cristianbrum.io/projects/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/cristianbrum.io/projects/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/cristianbrum.io/projects/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/cristianbrum.io/projects/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/cristianbrum.io/projects/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/cristianbrum.io/projects/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/cristianbrum.io/projects/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/cristianbrum.io/projects/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
